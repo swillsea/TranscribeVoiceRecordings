d8fe30139d86b0c18eb3820aff8886f71ff94b9f
Sam Willsea <sam.willsea@zaplabs.com>
2016-12-30 18:18:31 -0800

    Add data source loading

--- TranscribeVoiceRecordings/MemoriesViewController.swift
+++ TranscribeVoiceRecordings/MemoriesViewController.swift
@@ -12,11 +12,12 @@ import Speech
 import Photos
 
 class MemoriesViewController: UICollectionViewController {
-
+    
+    var memories = [URL]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        loadMemories()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -24,6 +25,34 @@ class MemoriesViewController: UICollectionViewController {
         checkPermissions()
     }
 
+    func loadMemories() {
+        memories.removeAll()
+        
+        // attempt loading all memories
+        guard let files = try? FileManager.default.contentsOfDirectory(at: getDocumentsDirectory(), includingPropertiesForKeys: nil, options: []) else {
+            return
+        }
+        
+        // add unique memories
+        for file in files {
+            let filename = file.lastPathComponent
+            if filename.hasSuffix(".thumb") {
+                let rootPath = filename.replacingOccurrences(of: ".thumb", with: "")
+                let memoryPath = getDocumentsDirectory().appendingPathComponent(rootPath)
+                memories.append(memoryPath)
+            }
+        }
+        
+        // only reload memories - ignore section[0] (search bar)
+        collectionView?.reloadSections(IndexSet(integer: 1))
+    }
+
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
     func checkPermissions() {
         let photosAuthorized = PHPhotoLibrary.authorizationStatus() == .authorized
         let recordingAuthorized = AVAudioSession.sharedInstance().recordPermission() == .granted
