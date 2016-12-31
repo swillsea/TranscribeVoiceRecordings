bce6dd85cfba85fe105159d1457671074660eafd
Sam Willsea <sam.willsea@zaplabs.com>
2016-12-30 17:14:00 -0800

    Set up Interface Builder

---
+++ TranscribeVoiceRecordings/MemoriesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MemoriesViewController.swift
+//  TranscribeVoiceRecordings
+//
+//  Created by Sam Willsea on 12/30/16.
+//  Copyright © 2016 Sam Willsea. All rights reserved.
+//
+
+import UIKit
+
+class MemoriesViewController: UICollectionViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ TranscribeVoiceRecordings/MemoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemoryCell.swift
+//  TranscribeVoiceRecordings
+//
+//  Created by Sam Willsea on 12/30/16.
+//  Copyright © 2016 Sam Willsea. All rights reserved.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    
+}
